17517fe0f749ed58a4f84d704c6d2e62cf1295f4
Scacchetti <luiscacchetti07@gmail.com>
2024-12-07 10:45:05 -0300

    Adicionando Icones e Logos do projeto

--- LouisOS/obj/Debug/net8.0/LouisOS.AssemblyInfo.cs
+++ LouisOS/obj/Debug/net8.0/LouisOS.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LouisOS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+495234876bcd0a362c694288196e561cc61c86b3")]
 [assembly: System.Reflection.AssemblyProductAttribute("LouisOS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LouisOS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
